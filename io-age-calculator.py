120a7aaa50dd07b11f6157c97f695c990afb8e2a
Staver37 <102354895+Staver37@users.noreply.github.com>
2022-03-25 11:37:35 +0200

    Add files via upload

---
+++ io-age-calculator.py
@@ -0,0 +1,9 @@
+# calculating python's age
+
+current_year = input( " What year is it? " )  
+current_year = int ( current_year ) #Floating
+release_year= 1991      # hardcoding
+age = current_year - release_year          # ->int
+type(release_year)       
+print ( "Pyton language Age is:" , age , " year " )
+
